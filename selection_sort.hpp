b344dc1a52acc85a4ef2fc6e7b291d06a3ecce22
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2024-07-05 23:48:30 +0300

    Create selection_sort.hpp

---
+++ selection_sort.hpp
@@ -0,0 +1,31 @@
+/* Selection Sort is an algorithm for sorting an array
+Time complexity: O(n^2) - Worst case: O(n^2)
+Space complexity: O(1) */
+
+#include <cstddef>
+
+/* Swap two elements (access by pointers) */
+template <typename T>
+void swap(T* a, T* b) {
+    T c = *a;
+    *a = *b;
+    *b = c;
+}
+
+/* Sort an array of elements, for which the "<"" operator is defined,
+via the Selection Sort algorithm. The original array is modified.
+Order: ascending */
+template <typename T>
+void selection_sort(T* arr, size_t arr_length) {
+    for (size_t i = 0; i < arr_length - 1; ++i) {
+        T* min_ptr = arr + i;
+        for (size_t j = i; j < arr_length; ++j) {
+            if (arr[j] < *min_ptr) {
+                min_ptr = arr + j;
+            }
+        }
+        if (min_ptr != arr + i) {
+            swap(min_ptr, arr + i);
+        }
+    }
+}
