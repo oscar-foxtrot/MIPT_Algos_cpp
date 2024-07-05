5e4218bc47e63f9000283804c052771b8d45207a
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2024-07-05 23:37:36 +0300

    Create bubble_sort.hpp

---
+++ bubble_sort.hpp
@@ -0,0 +1,30 @@
+/* Bubble Sort is an algorithm for sorting an array
+Time complexity: O(n^2) - Worst case: O(n^2)
+Space complexity: O(1) */
+
+#include <cstddef>
+
+/* Swap two elements */
+template <typename T>
+void swap(T& a, T& b) {
+    T c = a;
+    a = b;
+    b = c;
+}
+
+/* Sort an array of elements, for which the "<"" operator is defined,
+via the Bubble Sort algorithm. The original array is modified.
+Order: ascending */
+template <typename T>
+void bubble_sort(T* arr, size_t arr_length) {
+    bool swapped;
+    do {
+        swapped = false;
+        for (size_t i = 1; i < arr_length; ++i) {
+            if (arr[i] < arr[i - 1]) {
+                swap(arr[i], arr[i - 1]);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
