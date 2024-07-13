529d6dae9fe80db1287a5b124d0ba6313a2fdb59
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2024-07-14 01:43:51 +0300

    Create stack.cpp

---
+++ examples/stack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "../stack.hpp"
+
+int main() {
+    Stack<int> stack;
+    stack.push(3);
+    stack.push(5);
+    stack.push(1);
+    std::cout << stack << std::endl;
+    std::cout << "Popped element: " << stack.pop() << std::endl;
+    std::cout << stack << std::endl;
+    stack.pop();
+    std::cout << stack << std::endl;
+    stack.pop();
+    std::cout << stack;
+    return 0;
+}
