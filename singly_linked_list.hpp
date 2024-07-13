7c598207443aee10768735aeb9dc3b615a6455e5
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2024-07-13 11:56:24 +0300

    Create singly_linked_list.hpp

---
+++ singly_linked_list.hpp
@@ -0,0 +1,58 @@
+/* Simple implementation of singly- and doubly- linked lists */
+
+template <typename T>
+class SinglyLinkedList {
+public:
+    SinglyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    ~SinglyLinkedList() {
+        // Destructor to clean up allocated memory
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+    void insert(const T& value) { // Add to the tail
+        if (!head) {
+            head = new Node(value);
+            tail = head;
+            return;
+        }
+        tail->next = new Node(value);
+        tail = tail->next;
+        tail->value = value;
+        tail->next = nullptr;
+    }
+
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& os, const SinglyLinkedList<U>& lst);
+
+private:
+    struct Node {
+        T value;
+        Node* next;
+        Node(const T& value) : value(value), next(nullptr) {}
+    };
+
+    Node* head; // Natural order preserved
+    Node* tail; // insertion - O(1)
+};
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const SinglyLinkedList<T>& lst) {
+    os << "SinglyLinkedList(";
+    typename SinglyLinkedList<T>::Node* current_node = lst.head;
+    if (lst.head) {
+        os << current_node->value;
+        while (current_node->next) {
+            os << ", ";
+            current_node = current_node->next;
+            os << current_node->value;
+        }
+    }
+    os << ")";
+    return os;
+}
